3b3c2bc2a835368556bb916188705b6a029ed9ac
Kishan Reddy <kishanreddy.kethu@gmail.com>
2014-01-24 23:00:43 +0530

    Working

--- ulam_spiral.c
+++ ulam_spiral.c
@@ -1,30 +1,54 @@
+/*
+This is a fun program to fill a square of given size with numbers starting from 1 in a spiral format.
+Logic: This particular code uses recursion and looping for filling a 2D array with the numbers in spiral form.
+AUTHOR:Kethu Harikishan Reddy
+Email: kishanreddy.kethu@gmail.com
+Version=1.0
+*/
 #include <stdio.h>
-void spiral();
-static int x=0,a[100][100],start=1;
+void spiral(int p);
+static int x=0,a[10][10],start=1;
 void main()
 {
 	int n;
 	printf("Please enter the number for the Ulam Spiral:\n");
 	scanf("%d",&n);
-	spiral(n,start);
+	spiral(n);
+	int i,j;
+	for (i=0;i<n;i++)
+	{	for (j=0;j<n;j++)
+				printf("%3d  ",a[i][j]);//printing each array element
+		printf("\n");
+	}
 }
-void spiral(int n,int start)
-{	int i;
-	for (i=x;i<n;i++)
+void spiral(int p)
+{	
+	if (p<=0)
+		return;
+	else if (p==1)
+	{
+		a[x][x]=start;
+		return;
+	}
+
+	int i;
+	for (i=x;i<x+p;i++) //Top Left to Top Right.
 	{
 		a[x][i]=start++;
 	}
-	for (i=(x+1);i<n;i++)
+	for (i=(x+1);i<x+p;i++) //Top Right to Bottom Right.
 	{
-		a[i][n-1]=start++;
+		a[i][x+p-1]=start++;
 	}
-	for (i=(n-2);i>=0;i--)
+	for (i=(x+p-2);i>=x;i--) //Bottom Right to Bottom Left.
 	{
-		a[n-1][i]=start++;
+		a[x+p-1][i]=start++;
 
 	}	
-	for (i=(n-2);i>=0;i--)
+	for (i=(x+p-2);i>x;i--) //Bottom Left to Top Left.
 	{
-		a[i][]
+		a[i][x]=start++;
 	}
+	x++;
+	spiral(p-2); //Recursive call to itself with decrease in square size.
 }
