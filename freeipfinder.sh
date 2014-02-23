1345ec65993eb50df7babae192730d37282d69d1
Kishan Reddy <kishanreddy.kethu@gmail.com>
2014-02-23 21:10:29 +0530

    Add new files

---
+++ freeipfinder.sh
@@ -0,0 +1,23 @@
+<<COMMENT
+DESCRIPTION: This is a program to list all the free ip addresses in your local subnet.
+USAGE: You can invoke this shell script by using bash or sh. You have to type "bash freeipfinder.sh subnetadress(10.3.8) startaddress endaddress"
+AUTHOR: Kethu Harikishan Reddy
+EMAIL: kishanreddy.kethu@gmail.com
+COMMENT
+#!/bin/sh
+subnet=$1
+start=$2
+end=$3
+c=0
+while [ $start -le $end ]
+do
+   nmap -sP -v $subnet.$start|grep "down" > /dev/null
+	if [ "$?" -eq 0 ]; then
+	        list[$c]=10.3.8.$start #This is my local subnet address in my hostelroom. Bits-Goa \m/.
+	        c=`expr $c + 1`
+	fi
+start=`expr $start + 1`
+done
+for (( i = 0; i < c; i++ )); do
+	echo ${list[$i]}
+done
