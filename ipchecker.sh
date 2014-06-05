b16aa24935ea1429d55dd725f6e27b073fc3d74d
Kishan Reddy <kishanreddy.kethu@gmail.com>
2014-06-05 15:31:23 +0530

    Change Mistake

--- ipchecker.sh
+++ ipchecker.sh
@@ -4,7 +4,7 @@ AUTHOR: Kethu Harikishan Reddy
 EMAIL: kishanreddy.kethu@gmail.com
 COMMENT
 echo "Please enter the interface for which you wish to check the ip for: (eth0 for ethernet and wlan0 for wireless generally)"
-echo "You can also type in the multihomed addresses like eth0:1 etc."
+echo "You can also type in virtual adapter addresses like eth0:1 etc."
 read i
 res=$(/sbin/ifconfig $i | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 echo "The ip assigned for $i is " $res
